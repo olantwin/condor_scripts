e6d6e8f6169fcd7c7b5e45044ca038cf818c82ad
Oliver Lantwin <Oliver.Lantwin@cern.ch>
2018-03-22 12:17:46 +0100

    Update

--- rec.sh
+++ rec.sh
@@ -9,9 +9,6 @@ NJOBS=$3
 if eos stat /eos/experiment/ship/user/olantwin/"$DIR"/"$LSB_JOBINDEX"/ship.conical.MuonBack-TGeant4_rec.root; then
 	echo "Target exists, nothing to do."
 	exit 0
-# elif [[ "$(lsb_release -r -s)" != 6.9 ]]
-# then
-# 	exit 1
 else
 	python2 "$FAIRSHIPRUN"/macro/ShipReco.py  -f root://eospublic.cern.ch//eos/experiment/ship/user/olantwin/"$DIR"/"$LSB_JOBINDEX"/ship.conical.MuonBack-TGeant4.root -g root://eospublic.cern.ch//eos/experiment/ship/user/olantwin/"$DIR"/geofile_full.conical.MuonBack-TGeant4.root
 	xrdcp ship.conical.MuonBack-TGeant4_rec.root root://eospublic.cern.ch//eos/experiment/ship/user/olantwin/"$DIR"/"$LSB_JOBINDEX"/
--- sim.sh
+++ sim.sh
@@ -6,8 +6,6 @@ DIR=$1
 ProcId=$2
 LSB_JOBINDEX=$((ProcId+1))
 MUONS=/eos/experiment/ship/data/Mbias/pythia8_Geant4-withCharm_onlyMuons_4magTarget.root
-# export EOSSHIP=root://eospublic.cern.ch/
-# export EOS_MGM_URL=root://eospublic.cern.ch
 NTOTAL=17786274
 NJOBS=$3
 TANK=6
@@ -18,9 +16,6 @@ FIRST=$(((NTOTAL/NJOBS)*(LSB_JOBINDEX-1)))
 if eos stat /eos/experiment/ship/user/olantwin/"$DIR"/"$LSB_JOBINDEX"/ship.conical.MuonBack-TGeant4.root; then
 	echo "Target exists, nothing to do."
 	exit 0
-# elif [[ "$(lsb_release -r -s)" != 6.9 ]]
-# then
-# 	exit 1
 else
 	python2 "$FAIRSHIPRUN"/macro/run_simScript.py --tankDesign $TANK --muShieldDesign $MUSHIELD --MuonBack --nEvents $N --firstEvent $FIRST --sameSeed 1 --seed 1 -f $MUONS --nuTauTargetDesign $ISHIP
 	xrdcp ship.conical.MuonBack-TGeant4.root root://eospublic.cern.ch//eos/experiment/ship/user/olantwin/"$DIR"/"$LSB_JOBINDEX"/
